fe0e1aa4a9fd11d443b98467bc79beeea78c9620
Krystian Panek <krystian.panek@cognifide.com>
2020-01-28 14:22:32 +0100

    Minor

--- src/main/kotlin/com/cognifide/gradle/common/CommonPlugin.kt
+++ src/main/kotlin/com/cognifide/gradle/common/CommonPlugin.kt
@@ -1,17 +1,16 @@
 package com.cognifide.gradle.common
 
+import com.cognifide.gradle.common.utils.using
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class CommonPlugin : Plugin<Project> {
 
-    override fun apply(project: Project) {
-        with(project) {
-            setupExtensions()
-        }
+    override fun apply(project: Project) = using(project) {
+        registerExtension()
     }
 
-    private fun Project.setupExtensions() {
+    private fun Project.registerExtension() {
         extensions.create(CommonExtension.NAME, CommonExtension::class.java, this)
     }
 
