2292d6a21a5a17014c87bc2bfd289f0c9a4cb665
Krystian Panek <krystian.panek@cognifide.com>
2020-11-23 12:01:52 +0100

    Base plugin application

--- src/main/kotlin/com/cognifide/gradle/common/CommonPlugin.kt
+++ src/main/kotlin/com/cognifide/gradle/common/CommonPlugin.kt
@@ -3,12 +3,16 @@ package com.cognifide.gradle.common
 import com.cognifide.gradle.common.utils.using
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.BasePlugin
 
 class CommonPlugin : Plugin<Project> {
 
     override fun apply(project: Project) = project.using {
+        if (common.prop.boolean("common.plugin.base") != false) {
+            plugins.apply(BasePlugin::class.java)
+        }
         val common = extensions.create(CommonExtension.NAME, CommonExtension::class.java, this)
-        if (common.prop.boolean("repository.jcenter") != false) {
+        if (common.prop.boolean("common.repository.jcenter") != false) {
             repositories.jcenter()
         }
     }
