56a0ce1108a18f53c9174f7a76e990d6340badeb
Krystian Panek <krystian.panek@wundermanthompson.com>
2021-11-30 21:18:44 +0100

    Jenkins case

--- src/main/kotlin/com/cognifide/gradle/common/CommonPlugin.kt
+++ src/main/kotlin/com/cognifide/gradle/common/CommonPlugin.kt
@@ -1,6 +1,7 @@
 package com.cognifide.gradle.common
 
 import com.cognifide.gradle.common.file.FileUtil
+import com.cognifide.gradle.common.utils.Formats
 import com.cognifide.gradle.common.utils.using
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -11,7 +12,7 @@ class CommonPlugin : Plugin<Project> {
     override fun apply(project: Project) = project.using {
         val common = extensions.create(CommonExtension.NAME, CommonExtension::class.java, this)
 
-        if (common.prop.boolean("common.path.check") != false) {
+        if (common.prop.boolean("common.path.check") ?: pathCheckDefault(project)) {
             val rootPath = FileUtil.systemPath(project.projectDir.absolutePath)
             val sanitizedPath = FileUtil.systemPath(FileUtil.sanitizePath(rootPath))
             if (sanitizedPath != rootPath) {
@@ -29,6 +30,16 @@ class CommonPlugin : Plugin<Project> {
         if (common.prop.boolean("common.repository.mavenCentral") != false) {
             repositories.mavenCentral()
         }
+    }
+
+    /**
+     * Exceptionally, disable path checking in some circumstances.
+     * Some CI/CD systems like Jenkins may allow spaces in paths.
+     */
+    private fun pathCheckDefault(project: Project): Boolean {
+        val normalizedPath = Formats.normalizePath(project.projectDir.absolutePath)
+        val excludedPathFragments = listOf("/jenkins/")
+        return excludedPathFragments.none { normalizedPath.contains(it) }
     }
 
     companion object {
