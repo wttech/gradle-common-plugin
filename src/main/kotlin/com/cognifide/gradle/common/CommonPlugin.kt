2c10622f3aed7a9cdc8ac3af3d79b69639bbaa6e
Krystian Panek <krystian.panek@wundermanthompson.com>
2021-11-30 21:26:40 +0100

    Path check smart default

--- src/main/kotlin/com/cognifide/gradle/common/CommonPlugin.kt
+++ src/main/kotlin/com/cognifide/gradle/common/CommonPlugin.kt
@@ -12,18 +12,9 @@ class CommonPlugin : Plugin<Project> {
     override fun apply(project: Project) = project.using {
         val common = extensions.create(CommonExtension.NAME, CommonExtension::class.java, this)
 
-        if (common.prop.boolean("common.path.check") ?: pathCheckDefault(project)) {
-            val rootPath = FileUtil.systemPath(project.projectDir.absolutePath)
-            val sanitizedPath = FileUtil.systemPath(FileUtil.sanitizePath(rootPath))
-            if (sanitizedPath != rootPath) {
-                throw CommonException(
-                    "Project path contains problematic characters!\n" +
-                            "Shell scripts could run improperly with such paths.\n" +
-                            "Consider relocating project - updating the path from '$rootPath' to '$sanitizedPath'."
-                )
-            }
+        if (common.prop.boolean("common.path.check") ?: checkPathEnabled(project)) {
+            checkPath(project)
         }
-
         if (common.prop.boolean("common.plugin.base") != false) {
             plugins.apply(BasePlugin::class.java)
         }
@@ -33,13 +24,37 @@ class CommonPlugin : Plugin<Project> {
     }
 
     /**
+     * Detect project paths that may be a root cause of hard to investigate later problems.
+     */
+    private fun checkPath(project: Project) {
+        val rootPath = FileUtil.systemPath(project.projectDir.absolutePath)
+        val sanitizedPath = FileUtil.systemPath(FileUtil.sanitizePath(rootPath))
+        if (sanitizedPath != rootPath) {
+            throw CommonException(
+                "Project path contains problematic characters!\n" +
+                        "Shell scripts could run improperly with such paths.\n" +
+                        "Consider relocating project - updating the path from '$rootPath' to '$sanitizedPath'."
+            )
+        }
+    }
+
+    /**
      * Exceptionally, disable path checking in some circumstances.
      * Some CI/CD systems like Jenkins may allow spaces in paths.
      */
-    private fun pathCheckDefault(project: Project): Boolean {
-        val normalizedPath = Formats.normalizePath(project.projectDir.absolutePath)
-        val excludedPathFragments = listOf("/jenkins/")
-        return excludedPathFragments.none { normalizedPath.contains(it) }
+    private fun checkPathEnabled(project: Project): Boolean {
+        val envVars = listOf("JENKINS_HOME")
+        if (envVars.any { System.getenv(it).isNotBlank() }) {
+            return false
+        }
+
+        val projectPath = Formats.normalizePath(project.projectDir.absolutePath)
+        val pathFragments = listOf("/jenkins/")
+        if (pathFragments.any { projectPath.contains(it) }) {
+            return false
+        }
+
+        return true
     }
 
     companion object {
